6aaee454d68b86925d92a9e275b73d1b6b3faba6
Alain D'EURVEILHER <alain.deurveilher@infrabel.be>
2021-09-20 14:05:36 +0200

    Fix the unit test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+import middleware from '../middleware';
 import App from './App';
 
 test('renders would you rather app', () => {
-  render(<App />);
+  const store = createStore(rootReducer, middleware);
+  render(<App />, {wrapper: ({children}) => <Provider store={store}>{children}</Provider>});
   const titleElement = screen.getByText(/would you rather/i);
   expect(titleElement).toBeInTheDocument();
 });
